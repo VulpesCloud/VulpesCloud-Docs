ebbfc401b551f327cd002937bdba4abff94050ee
TheCGuyGitHub <132585491+TheCGuyGitHub@users.noreply.github.com>
2025-05-24 17:14:12 +0200

    Initial Commit

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,25 +1,18 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from "next/image";
 
 /**
  * Shared layout configurations
  *
  * you can customise layouts individually from:
- * Home Layout: app/(home)/layout.tsx
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <Image src="https://avatars.githubusercontent.com/u/185675318" width="22" height="22" alt='' />
+        VulpesCloud Docs
       </>
     ),
   },
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [new URL('https://avatars.githubusercontent.com/u/185675318')],
+  },
 };
 
 export default withMDX(config);
